819f49fe1b5173aa47a9c016e34a536d3e7b4779
Sam Chung <samc@seek.com.au>
2022-12-06 12:57:25 +1100

    fix dynamic import

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-import { rules } from './rules';
+import { readdirSync } from 'node:fs';
+import path from 'node:path';
 
-const configuration = {
-  rules,
-};
+import { TSESLint } from '@typescript-eslint/utils';
+const rulesDir = path.join(__dirname, 'rules');
+const ruleFolders = readdirSync(rulesDir);
 
-export = configuration;
+type RuleModule = TSESLint.RuleModule<string, unknown[]> & { name: string };
+
+const rules = ruleFolders.reduce<Record<string, RuleModule>>((acc, curr) => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const rule = require(path.join(rulesDir, curr, 'rule')) as RuleModule;
+  acc[rule.name] = rule;
+  return acc;
+}, {});
+
+export = { rules };
