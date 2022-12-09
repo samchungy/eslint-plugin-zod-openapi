b122c9a6245b98162ba56aacf50bb68b0b28c3e7
Sam Chung <samc@seek.com.au>
2022-12-09 21:34:57 +1100

    reenable test

--- src/rules/require-comment/rule.test.ts
+++ src/rules/require-comment/rule.test.ts
@@ -17,90 +17,90 @@ const ruleTester = new ESLintUtils.RuleTester({
 
 ruleTester.run(ruleName, rule, {
   valid: [
-    // test('string-description'),
-    // test('object-property-description'),
-    // test('string-deprecated'),
-    // test('object-property-deprecated'),\
-    test('string-inline-comment'),
+    test('string-description'),
+    test('object-property-description'),
+    test('string-deprecated'),
+    test('object-property-deprecated'),
+    // test('string-inline-comment'),
   ],
   invalid: [
-    // {
-    //   ...test('string-description-wrong-comment', true),
-    //   errors: [{ messageId: 'comment' }],
-    // },
-    // {
-    //   ...test('string-no-description'),
-    //   errors: [{ messageId: 'required' }],
-    // },
-    // {
-    //   ...test('string-description-no-comment', true),
-    //   errors: [{ messageId: 'comment' }],
-    // },
-    // {
-    //   ...test('string-description-wrong-comment-indent', true),
-    //   errors: [{ messageId: 'comment' }],
-    // },
-    // {
-    //   ...test('string-description-no-comment-indent', true),
-    //   errors: [{ messageId: 'comment' }],
-    // },
-    // {
-    //   ...test('object-property-description-no-comment', true),
-    //   errors: [{ messageId: 'comment' }],
-    // },
-    // {
-    //   ...test(
-    //     'object-property-description-no-comment-same-line-disable-prettier',
-    //     true,
-    //   ),
-    //   errors: [{ messageId: 'comment' }],
-    // },
-    // {
-    //   ...test(
-    //     'object-property-description-no-comment-same-line-prop-disable-prettier',
-    //     true,
-    //   ),
-    //   errors: [{ messageId: 'comment' }],
-    // },
-    // {
-    //   ...test('object-property-wrong-description', true),
-    //   errors: [{ messageId: 'comment' }],
-    // },
-    // {
-    //   ...test('string-deprecated-no-comment', true),
-    //   errors: [{ messageId: 'comment' }],
-    // },
-    // {
-    //   ...test('string-deprecated-wrong-comment', true),
-    //   errors: [{ messageId: 'comment' }],
-    // },
-    // {
-    //   ...test('object-property-description-no-deprecated', true),
-    //   errors: [{ messageId: 'comment' }],
-    // },
-    // {
-    //   ...test('string-description-remove-deprecated', true),
-    //   errors: [{ messageId: 'comment' }],
-    // },
-    // {
-    //   ...test('object-property-remove-deprecated', true),
-    //   errors: [{ messageId: 'comment' }],
-    // },
-    // {
-    //   ...test('string-deprecation-change-description', true),
-    //   errors: [{ messageId: 'comment' }],
-    // },
-    // {
-    //   ...test('string-referenced', true),
-    //   errors: [{ messageId: 'comment' }],
-    // },
-    // {
-    //   ...test('object-property-referenced', true),
-    //   errors: [{ messageId: 'comment' }],
-    // },
-    // {
-    //   ...test('string-referenced-deprecated', true),
-    //   errors: [{ messageId: 'comment' }],
-    // },
+    {
+      ...test('string-description-wrong-comment', true),
+      errors: [{ messageId: 'comment' }],
+    },
+    {
+      ...test('string-no-description'),
+      errors: [{ messageId: 'required' }],
+    },
+    {
+      ...test('string-description-no-comment', true),
+      errors: [{ messageId: 'comment' }],
+    },
+    {
+      ...test('string-description-wrong-comment-indent', true),
+      errors: [{ messageId: 'comment' }],
+    },
+    {
+      ...test('string-description-no-comment-indent', true),
+      errors: [{ messageId: 'comment' }],
+    },
+    {
+      ...test('object-property-description-no-comment', true),
+      errors: [{ messageId: 'comment' }],
+    },
+    {
+      ...test(
+        'object-property-description-no-comment-same-line-disable-prettier',
+        true,
+      ),
+      errors: [{ messageId: 'comment' }],
+    },
+    {
+      ...test(
+        'object-property-description-no-comment-same-line-prop-disable-prettier',
+        true,
+      ),
+      errors: [{ messageId: 'comment' }],
+    },
+    {
+      ...test('object-property-wrong-description', true),
+      errors: [{ messageId: 'comment' }],
+    },
+    {
+      ...test('string-deprecated-no-comment', true),
+      errors: [{ messageId: 'comment' }],
+    },
+    {
+      ...test('string-deprecated-wrong-comment', true),
+      errors: [{ messageId: 'comment' }],
+    },
+    {
+      ...test('object-property-description-no-deprecated', true),
+      errors: [{ messageId: 'comment' }],
+    },
+    {
+      ...test('string-description-remove-deprecated', true),
+      errors: [{ messageId: 'comment' }],
+    },
+    {
+      ...test('object-property-remove-deprecated', true),
+      errors: [{ messageId: 'comment' }],
+    },
+    {
+      ...test('string-deprecation-change-description', true),
+      errors: [{ messageId: 'comment' }],
+    },
+    {
+      ...test('string-referenced', true),
+      errors: [{ messageId: 'comment' }],
+    },
+    {
+      ...test('object-property-referenced', true),
+      errors: [{ messageId: 'comment' }],
+    },
+    {
+      ...test('string-referenced-deprecated', true),
+      errors: [{ messageId: 'comment' }],
+    },
   ],
 });
