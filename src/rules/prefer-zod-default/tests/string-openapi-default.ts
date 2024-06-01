d60a47e20d1595337bbb34964d2a7d2441869a93
samchungy <samchungy@gmail.com>
2024-06-01 13:03:53 +1000

    update imports

--- src/rules/prefer-zod-default/tests/string-openapi-default.ts
+++ src/rules/prefer-zod-default/tests/string-openapi-default.ts
@@ -1,6 +1,4 @@
+import 'zod-openapi/extend';
 import { z } from 'zod';
-import { extendZodWithOpenApi } from 'zod-openapi';
-
-extendZodWithOpenApi(z);
 
 export const ZodString = z.string().openapi({ default: 'default' });
--- src/rules/require-example/tests/boolean-no-example.ts
+++ src/rules/require-example/tests/boolean-no-example.ts
@@ -1,6 +1,4 @@
+import 'zod-openapi/extend';
 import { z } from 'zod';
-import { extendZodWithOpenApi } from 'zod-openapi';
-
-extendZodWithOpenApi(z);
 
 export const ZodPrimative = z.boolean().openapi({ description: 'test' });
